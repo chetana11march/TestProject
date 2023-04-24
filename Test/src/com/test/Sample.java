5694864aabe3de9edf2a7b630d23f1fac1523e29
chetana11march <131536350+chetana11march@users.noreply.github.com>
2023-04-24 07:59:46 +0530

    Added line by remote

--- Test/src/com/test/Sample.java
+++ Test/src/com/test/Sample.java
@@ -2,7 +2,8 @@ package com.test;
 
 public class Sample {
 	public static void main(String[] args) {
-		System.out.println("1st line added");
+		System.out.println("1st line added by local");
+		System.out.println("1st line added by remote");
 	}
 
 }
