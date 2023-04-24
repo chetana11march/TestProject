cfc8ae01726a11788a175b7a258fcb42d048259e
chetana11march <131536350+chetana11march@users.noreply.github.com>
2023-04-24 08:34:52 +0530

    Update Sample.java

--- Test/src/com/test/Sample.java
+++ Test/src/com/test/Sample.java
@@ -4,6 +4,10 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("1st line added by local");
 		System.out.println("1st line added by remote");
+		System.out.println("1st line added by local");
+		System.out.println("1st line added by remote");
+		System.out.println("1st line added by local");
+		System.out.println("1st line added by remote");
 	}
 
 }
